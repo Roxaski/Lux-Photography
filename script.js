d4911429da81e94d3d98e9da7bb87da6f6f1b943
Roxas Ski <roxaskiGithub@gmail.com>
2026-03-24 16:27:11 +0000

    rewrote the code for improved accessibility and readability

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-let nav = document.querySelector('nav');
-let menu = document.querySelector('.hamburger-menu');
+const nav = document.querySelector('nav');
+const links = document.querySelector('.links');
+const menu = document.querySelector('.hamburger-menu');
+const main = document.querySelector('main');
+const hero = document.querySelector('.hero-section');
+const mobileHeader = document.querySelector('.mobile-header');
 
-// toggles the hamburger menu
-menu.addEventListener('click', () => {
-    nav.classList.toggle('active')
-});
-
-// disables scrolling when the hamburger menu is active
-menu.addEventListener('click', () => {
+menu.addEventListener('click', toggleHamburgerMenu);
+// toggles the hamburger menu, along with disabling scroll when menu is open
+function toggleHamburgerMenu() {
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
     document.body.classList.toggle('no-scroll');
-});
+    // prevents these elements from being focused, clicked, or read by screen readers
+    hero.inert = active;
+    main.inert = active;
+
+    // if the this element exists, and if it does it prevents it from being focused 
+    if (mobileHeader) {
+        mobileHeader.inert = active;
+    };
+
+    if (active) {
+        document.addEventListener('keydown', escapeKeyPress);
+    } else {
+        document.removeEventListener('keydown', escapeKeyPress);
+        menu.focus();
+    };
+};
+
+function escapeKeyPress(e) {
+    if (e.key === 'Escape') {
+        toggleHamburgerMenu();
+    };
+};
